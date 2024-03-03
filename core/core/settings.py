b4d3808fa43d44aab361f1eb3ce46057c47bab32
danial sadri <danielsadri01@gmail.com>
2024-03-03 17:54:40 +0330

    update

--- core/core/settings.py
+++ core/core/settings.py
@@ -1,13 +1,12 @@
 from pathlib import Path
 from decouple import config
 
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='test')
 DEBUG = config('DEBUG', cast=bool, default=True)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')], default='*')
 
-# Application definition
+# Application definition 
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
