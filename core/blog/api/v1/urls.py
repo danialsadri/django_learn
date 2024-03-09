2c717d66dffd0120c9fdb47899e7eab5e106c6eb
danial sadri <danielsadri01@gmail.com>
2024-03-09 12:28:24 +0330

    update

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -3,9 +3,19 @@ from . import views
 
 app_name = 'api-v1'
 urlpatterns = [
+    # fbv
     path('post-list/', views.post_list, name='post-list'),
     path('post-detail/<int:pk>/', views.post_detail, name='post-detail'),
     path('post-create/', views.post_create, name='post-create'),
     path('post-update/<int:pk>/', views.post_update, name='post-update'),
     path('post-delete/<int:pk>/', views.post_delete, name='post_delete'),
+    # cbv
+    path('post-list-cbv/', views.PostListView.as_view(), name='post-list-cbv'),
+    path('post-detail-cbv/<int:pk>/', views.PostDetailView.as_view(), name='post-detail-cbv'),
+    path('post-create-cbv/', views.PostCreateView.as_view(), name='post-create-cbv'),
+    path('post-update-cbv/<int:pk>/', views.PostUpdateView.as_view(), name='post-update-cbv'),
+    path('post-delete-cbv/<int:pk>/', views.PostDeleteView.as_view(), name='post_delete-cbv'),
+    # generic
+    path('post-list-create-generic/', views.PostListCreateView.as_view(), name='post-list-create-generic'),
+    path('post-retrieve-update-destroy-generic/<int:post_id>/', views.PostRetrieveUpdateDetailView.as_view(), name='post-retrieve-update-destroy-generic'),
 ]
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -3,11 +3,15 @@ from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
 from .serializers import (PostListSerializer, PostDetailSerializer,
                           PostUpdateSerializer, PostCreateSerializer)
 from ...models import Post
 
 
+# =================================================================================================================
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def post_list(request):
@@ -49,3 +53,160 @@ def post_delete(request, pk):
     post = get_object_or_404(Post, id=pk, status=True)
     post.delete()
     return Response({'message': 'item removed successfully'}, status=status.HTTP_204_NO_CONTENT)
+
+
+# =================================================================================================================
+
+
+# =================================================================================================================
+class PostListView(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PostListSerializer
+
+    def get(self, request):
+        posts = Post.objects.filter(status=True)
+        serializer = self.serializer_class(instance=posts, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+class PostDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PostDetailSerializer
+
+    def get(self, request, pk):
+        post = get_object_or_404(Post, id=pk, status=True)
+        serializer = self.serializer_class(instance=post)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+class PostCreateView(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PostCreateSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+
+class PostUpdateView(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PostUpdateSerializer
+
+    def patch(self, request, pk):
+        post = get_object_or_404(Post, id=pk, status=True)
+        serializer = self.serializer_class(instance=post, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(data=serializer.data, status=status.HTTP_202_ACCEPTED)
+
+
+class PostDeleteView(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = None
+
+    def delete(self, request, pk):
+        post = get_object_or_404(Post, id=pk, status=True)
+        post.delete()
+        return Response({'message': 'item removed successfully'}, status=status.HTTP_204_NO_CONTENT)
+
+
+# =================================================================================================================
+
+
+# =================================================================================================================
+# class PostListCreateView(GenericAPIView):
+#     queryset = Post.objects.filter(status=True)
+#     serializer_class = PostListSerializer
+#     permission_classes = [IsAuthenticated]
+#
+#     def get(self, request):
+#         queryset = self.get_queryset()
+#         serializer = self.serializer_class(instance=queryset, many=True)
+#         return Response(data=serializer.data, status=status.HTTP_200_OK)
+#
+#     def post(self, request):
+#         serializer = self.serializer_class(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+
+# class PostListCreateView(GenericAPIView, ListModelMixin, CreateModelMixin):
+#     queryset = Post.objects.filter(status=True)
+#     serializer_class = PostListSerializer
+#     permission_classes = [IsAuthenticated]
+#
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+#
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+
+
+class PostListCreateView(ListCreateAPIView):
+    queryset = Post.objects.filter(status=True)
+    serializer_class = PostListSerializer
+    permission_classes = [IsAuthenticated]
+
+
+# =================================================================================================================
+# class PostRetrieveUpdateDetailView(GenericAPIView):
+#     serializer_class = PostDetailSerializer
+#     permission_classes = [IsAuthenticated]
+#     lookup_field = 'id'
+#     lookup_url_kwarg = 'post_id'
+#
+#     def get_object(self):
+#         pk = self.kwargs.get('post_id')
+#         return get_object_or_404(Post, id=pk, status=True)
+#
+#     def get(self, request, *args, **kwargs):
+#         post = self.get_object()
+#         serializer = self.serializer_class(instance=post)
+#         return Response(data=serializer.data, status=status.HTTP_200_OK)
+#
+#     def patch(self, request, *args, **kwargs):
+#         post = self.get_object()
+#         serializer = self.serializer_class(instance=post, data=request.data, partial=True)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(data=serializer.data, status=status.HTTP_202_ACCEPTED)
+#
+#     def delete(self, request, *args, **kwargs):
+#         post = self.get_object()
+#         post.delete()
+#         return Response({'message': 'item removed successfully'}, status=status.HTTP_204_NO_CONTENT)
+
+
+# class PostRetrieveUpdateDetailView(GenericAPIView, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
+#     serializer_class = PostDetailSerializer
+#     permission_classes = [IsAuthenticated]
+#     lookup_field = 'id'
+#     lookup_url_kwarg = 'post_id'
+#
+#     def get_object(self):
+#         pk = self.kwargs.get('post_id')
+#         return get_object_or_404(Post, id=pk, status=True)
+#
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)
+#
+#     def patch(self, request, *args, **kwargs):
+#         return self.partial_update(request, *args, **kwargs)
+#
+#     def delete(self, request, *args, **kwargs):
+#         return self.destroy(request, *args, **kwargs)
+
+
+class PostRetrieveUpdateDetailView(RetrieveUpdateDestroyAPIView):
+    serializer_class = PostDetailSerializer
+    permission_classes = [IsAuthenticated]
+    lookup_field = 'id'
+    lookup_url_kwarg = 'post_id'
+
+    def get_object(self):
+        pk = self.kwargs.get('post_id')
+        return get_object_or_404(Post, id=pk, status=True)
+# =================================================================================================================
